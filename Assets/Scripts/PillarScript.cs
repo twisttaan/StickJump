5f53b492a7e025a227b4f2121a9cb6089b55f59c
Tristan Camejo <twisttaan@icloud.com>
2022-04-02 22:52:07 +1100

    feat: pillars

---
+++ Assets/Scripts/PillarScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarScript : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    void Update()
+    {
+        rb2D.velocity = new Vector2(-4, 0);
+    }
+}
